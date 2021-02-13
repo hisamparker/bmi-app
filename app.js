ed8d07f6abb9bc51622b34beb5e0bfb9426e4d85
hisamparker <hisamparker@gmail.com>
2021-02-13 12:47:59 +0100

    added requiremenst to js doc

---
+++ app.js
@@ -0,0 +1,19 @@
+//  REQUIREMENTS
+// calculate and print user's ideal weight
+// calculate and print if user is under or over weight
+// calculate and print daily caloric burn w / w out exercise and depending on sex
+// calculate and print daily calories needed for steady weight loss / gain
+// calculate and print how long weight change will take
+// create validations and print error message if data is incorrectly entered
+
+// The formula for BMI is weigh (kg) / (height (m) x height (m))
+// A healthy range of BMI is 18.5 to 25
+
+// BUSINESS LOGIC
+// We need a bmi calculator + ideal weight calculator, bmr calculator, tdee calculator, and diet plan calculator
+// We need the user's gender, activity level, weight in kg, and height in m /cm
+// We need standard bmi, bmr, and tdee calculations as well as data on ideal weight and weight change calories
+ 
+
+
+
